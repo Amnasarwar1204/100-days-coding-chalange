86dcbd21ee375c0db4192279dcd987950fb45b37
Amnasarwar1204 <157794672+Amnasarwar1204@users.noreply.github.com>
2024-06-30 20:10:44 +0500

    first commit

---
+++ day46/q136/main.js
@@ -0,0 +1,6 @@
+// Demonstrates using console.log() inside a loop to track variable values
+for (var i = 0; i < 10; i++) {
+    console.log("Iteration: ".concat(i, " , i value:, i"));
+}
+// Logs the current iteration number and the value of i
+// This loop iterates five times, logging the value of 'i' during each iteration to help with debugging.
---
+++ day46/q136/main.ts
@@ -0,0 +1,9 @@
+// Demonstrates using console.log() inside a loop to track variable values
+for (let i = 0; i < 10; i++) {
+
+    console.log(`Iteration: ${i} , i value:, i`)// Logs the current iteration number and the value of i
+    
+} 
+
+  
+  // This loop iterates five times, logging the value of 'i' during each iteration to help with debugging.
